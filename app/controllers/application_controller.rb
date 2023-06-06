4beb583013976a2784cbb568a2901bed0966cc9c
Michal <khakaimic4@gmail.com>
2023-06-06 12:45:49 +0300

    post requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,24 @@ class ApplicationController < Sinatra::Base
     trains = Train.all 
     trains.to_json
   end
-end
+
+  get "/buses/:id" do
+    bus = Bus.find(params[:id])
+    bus.to_json
+  end
+
+  get "/trains/:id" do
+    train = Train.find(params[:id])
+    train.to_json
+  end
+
+  post "/buses" do
+    bus = Bus.create(bus_number:params[:bus_number], departure_city:params[:departure_city], destination_city:params[:destination_city], departure:params[:departure], arrival:params[:arrival], available_seats:params[:available_seats], fare:params[:fare])
+    bus.to_json
+  end
+
+  post "/trains" do
+    train = Train.create(train_number:params[:train_number], departure_city:params[:departure_city], destination_city:params[:destination_city], departure:params[:departure], arrival:params[:arrival], available_seats:params[:available_seats], fare:params[:fare])
+    train.to_json
+  end  
+  end
