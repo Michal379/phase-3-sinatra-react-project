ceaef8fabc2bdd719d93c136aa56d16034802ab8
Michal <khakaimic4@gmail.com>
2023-06-06 15:33:41 +0300

    patch request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  #get requests
+
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
@@ -16,6 +18,12 @@ class ApplicationController < Sinatra::Base
     trains.to_json
   end
 
+  get "/customers" do
+    customers = Customer.all
+    customers.to_json
+  end
+
+  #get by id
   get "/buses/:id" do
     bus = Bus.find(params[:id])
     bus.to_json
@@ -26,13 +34,90 @@ class ApplicationController < Sinatra::Base
     train.to_json
   end
 
+  get "/customers/:id" do
+    customer = Customer.find(params[:id])
+    customer.to_json
+  end
+
+  #post requests
   post "/buses" do
-    bus = Bus.create(bus_number:params[:bus_number], departure_city:params[:departure_city], destination_city:params[:destination_city], departure:params[:departure], arrival:params[:arrival], available_seats:params[:available_seats], fare:params[:fare])
+    bus = Bus.create(
+      bus_number:params[:bus_number], 
+      departure_city:params[:departure_city], 
+      destination_city:params[:destination_city], 
+      departure:params[:departure], 
+      arrival:params[:arrival], 
+      available_seats:params[:available_seats], 
+      fare:params[:fare]
+      )
     bus.to_json
   end
 
   post "/trains" do
-    train = Train.create(train_number:params[:train_number], departure_city:params[:departure_city], destination_city:params[:destination_city], departure:params[:departure], arrival:params[:arrival], available_seats:params[:available_seats], fare:params[:fare])
+    train = Train.create(
+      train_number:params[:train_number], 
+      departure_city:params[:departure_city], 
+      destination_city:params[:destination_city], 
+      departure:params[:departure], 
+      arrival:params[:arrival], 
+      available_seats:params[:available_seats], 
+      fare:params[:fare]
+    )
     train.to_json
-  end  
+  end 
+
+  post "/customers" do
+    customer = Customer.create(
+      first_name:params[:first_name],
+      last_name:params[:last_name],
+      passport:params[:passport],
+      phone_number:params[:phone_number],
+      mode_of_payment:params[:mode_of_payment],
+      departure_info:params[:departure_info]
+    )
+    customer.to_json
+  end
+
+  # patch requests
+  patch '/buses/:id' do
+    bus = Bus.find(params[:id])
+    bus.update(
+      bus_number:params[:bus_number], 
+      departure_city:params[:departure_city], 
+      destination_city:params[:destination_city], 
+      departure:params[:departure], 
+      arrival:params[:arrival], 
+      available_seats:params[:available_seats], 
+      fare:params[:fare]
+    )
+    bus.to_json
+     end
+
+     patch '/trains/:id' do
+      train = Train.find(params[:id])
+    train.update(
+      train_number:params[:train_number], 
+      departure_city:params[:departure_city], 
+      destination_city:params[:destination_city], 
+      departure:params[:departure], 
+      arrival:params[:arrival], 
+      available_seats:params[:available_seats], 
+      fare:params[:fare]
+    )
+    train.to_json
+     end
+
+     patch '/customers/:id' do
+      customer = Customer.find(params[:id])
+      customer.update(
+      first_name:params[:first_name],
+       last_name:params[:last_name],
+       passport:params[:passport],
+       phone_number:params[:phone_number],
+       mode_of_payment:params[:mode_of_payment],
+       departure_info:params[:departure_info]
+      )
+      customer.to_json
+     end
+
   end
